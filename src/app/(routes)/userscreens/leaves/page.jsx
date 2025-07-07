d5565c700ca60901f78f97a853293f1547afd8b1
“vsn20” <vuppalasainaman@gmail.com>
2025-07-07 21:57:22 +0530

    db configuration

---
+++ src/app/(routes)/userscreens/leaves/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Overview from '@/app/components/Leaves/Overview'
+const page = () => {
+  return (
+    <div><Overview/></div>
+  )
+}
+
+export default page
---
+++ src/app/components/Leaves/Overview.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Overview = () => {
+  return (
+    <div>Leaves</div>
+  )
+}
+
+export default Overview
