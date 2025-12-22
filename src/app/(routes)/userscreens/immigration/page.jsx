7dc35d96c6d23e7b329227edc6754b5524d1e2bf
vsn_20 <vuppalasainaman@gmail.com>
2025-12-23 04:46:50 +0530

    cgan

--- src/app/(routes)/userscreens/immigration/page.jsx
+++ src/app/(routes)/userscreens/immigration/page.jsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import React from 'react';
 import Immigration from '@/app/components/Immigration/Immigration';
 import DBconnection from '@/app/utils/config/db';
